671f69c5bb6859910dbad4302f1e20fbe2ccf9db
SemK06 <semspammm@gmail.com>
2021-01-21 17:06:57 +0100

    Update 0.1.1

--- index.js
+++ index.js
@@ -7,8 +7,31 @@ const bot = new discord.Client();
 bot.on("ready", async () => {
 
     console.log(`${bot.user.username} is wakker!`) 
+
+    bot.user.setActivity("Pluimpje Development", {type: "PLAYING"})
     
 })
 
+bot.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === "dm") return;
+    
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+    if( command === `${prefix}serverlog`){
+        
+        return message.channel.send("Server logs komen meestal aan het einde van de dag. Wacht geduldig af!");
+        
+    }
+
+})
 
 bot.login(process.env.token);
