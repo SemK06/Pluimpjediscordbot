949ebb40db0bf6e345a997ea264918075e121636
SemK06 <semspammm@gmail.com>
2021-01-21 16:51:00 +0100

    Test

---
+++ index.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const bot = new discord.Client();
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is wakker!`) 
+    
+})
+
+
+bot.login(botConfig.token);
